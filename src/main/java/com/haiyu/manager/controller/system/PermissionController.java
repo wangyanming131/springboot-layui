9d7dbad4ceb4aa35364902d679bb88f364f76271
wangyanming131 <wangyanming131@126.com>
2021-06-08 17:15:53 +0800

    路径

--- src/main/java/com/haiyu/manager/controller/system/PermissionController.java
+++ src/main/java/com/haiyu/manager/controller/system/PermissionController.java
@@ -44,7 +44,7 @@ public class PermissionController {
     @RequestMapping("permissionManage")
     public String permissionManage() {
         logger.info("进入权限管理");
-        return "/permission/permissionManage";
+        return "/base/permission/permissionManage";
     }
 
 
